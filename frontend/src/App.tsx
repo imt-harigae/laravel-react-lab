eb0fc97c5fe6984059622940a5f17333f969e902
imt-harigae <d-harigae@imt-mirai.co.jp>
2025-06-02 10:31:12 +0900

    feat: レイアウト追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
 import TodoList from './pages/TodoList';
 import TodoEdit from './pages/TodoEdit';
 import Tailwind from './pages/Tailwind';
@@ -6,15 +8,19 @@ import Tailwind from './pages/Tailwind';
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Todoリストのページ */}
-        <Route path="/" element={<TodoList />} />
-        {/* Todo編集のページ */}
-        <Route path="/edit/:id" element={<TodoEdit />} />
+      <Layout>
+        <Routes>
+          {/* Home */}
+          <Route path="/" element={<Home />} />
 
-        {/* Tailwind動作確認用のページ */}
-        <Route path="/tailwind" element={<Tailwind />} />
-      </Routes>
+          {/* Todo */}
+          <Route path="/todos" element={<TodoList />} />
+          <Route path="/edit/:id" element={<TodoEdit />} />
+
+          {/* Tailwind */}
+          <Route path="/tailwind" element={<Tailwind />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
